0b6ad50093dd27a86d8935bab10259583f9645f7
bombadil1984 <joukevanleeuwen@gmail.com>
2020-10-28 23:48:51 +0100

    REACT
    full tutorial todo-list

---
+++ react_to_do/src/components/layout/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <header style={headerStyle}>
+            <h1>TodoList</h1>
+            <Link style={linkStyle} to="/">Home</Link> ||
+            <Link style={linkStyleTwo}
+            to="/about">About</Link>
+        </header>
+    )
+}
+
+const headerStyle = {
+    background: '#333',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '10px'
+}
+
+const linkStyle = {
+    color: '#fff',
+    textDecoration: 'none'
+    
+}
+
+const linkStyleTwo = {
+    color: '#fff',
+    textDecoration: 'none',
+    marginLeft: '5px'
+    
+}
+
+export default Header;
